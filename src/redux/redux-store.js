2d5c6617f38239e4a96c066f24b9512ee8b7b3bc
Sendega89 <78374273+Sendega89@users.noreply.github.com>
2023-05-02 00:48:29 +0300

    fgh
    dfh

---
+++ src/redux/redux-store.js
@@ -0,0 +1,25 @@
+import {applyMiddleware, combineReducers, compose, createStore} from "redux";
+import thunkMeddleWare from "redux-thunk";
+import page_Reducer from "./page_Reducer";
+import profile_Reducer from "@/redux/profile_Reducer";
+import directory_Reducer from "@/redux/directory_Reducer";
+
+
+
+
+let reducers = combineReducers({
+    page:page_Reducer,
+    profilePage: profile_Reducer,
+    directory:directory_Reducer,
+});
+
+
+const composeEnhancers = process.browser && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    : compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunkMeddleWare)
+));
+
+
+
+export default store;
